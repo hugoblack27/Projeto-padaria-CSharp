dbcf5ff989bfc9b4e1b382b43eb770888a334892
hugoblack27 <hugohenrique9341@gmail.com>
2025-04-10 11:52:56 -0300

    Aprendendo Orientação de Objetos

---
+++ ProjetoOrientacaoObjeto/frmEstruturaRepiticao.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ProjetoOrientacaoObjeto
+{
+    partial class frmEstruturaRepiticao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCarregaEstado = new System.Windows.Forms.Button();
+            this.btnCarregarListadeDesejo = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblListadeEstados = new System.Windows.Forms.Label();
+            this.ltbListaDesejos = new System.Windows.Forms.ListBox();
+            this.cbbEstados = new System.Windows.Forms.ComboBox();
+            this.lblDigiteEstado = new System.Windows.Forms.Label();
+            this.txtInserirEstado = new System.Windows.Forms.TextBox();
+            this.btnInserirEstado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCarregaEstado
+            // 
+            this.btnCarregaEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregaEstado.Location = new System.Drawing.Point(67, 388);
+            this.btnCarregaEstado.Name = "btnCarregaEstado";
+            this.btnCarregaEstado.Size = new System.Drawing.Size(144, 50);
+            this.btnCarregaEstado.TabIndex = 3;
+            this.btnCarregaEstado.Text = "Carrega Estado";
+            this.btnCarregaEstado.UseVisualStyleBackColor = true;
+            // 
+            // btnCarregarListadeDesejo
+            // 
+            this.btnCarregarListadeDesejo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregarListadeDesejo.Location = new System.Drawing.Point(241, 388);
+            this.btnCarregarListadeDesejo.Name = "btnCarregarListadeDesejo";
+            this.btnCarregarListadeDesejo.Size = new System.Drawing.Size(176, 50);
+            this.btnCarregarListadeDesejo.TabIndex = 4;
+            this.btnCarregarListadeDesejo.Text = "Carregar Lista Estado";
+            this.btnCarregarListadeDesejo.UseVisualStyleBackColor = true;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(64, 26);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(60, 20);
+            this.lblEstado.TabIndex = 2;
+            this.lblEstado.Text = "Estado";
+            // 
+            // lblListadeEstados
+            // 
+            this.lblListadeEstados.AutoSize = true;
+            this.lblListadeEstados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListadeEstados.Location = new System.Drawing.Point(469, 35);
+            this.lblListadeEstados.Name = "lblListadeEstados";
+            this.lblListadeEstados.Size = new System.Drawing.Size(128, 20);
+            this.lblListadeEstados.TabIndex = 3;
+            this.lblListadeEstados.Text = "Lista de Estados";
+            // 
+            // ltbListaDesejos
+            // 
+            this.ltbListaDesejos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltbListaDesejos.FormattingEnabled = true;
+            this.ltbListaDesejos.ItemHeight = 20;
+            this.ltbListaDesejos.Location = new System.Drawing.Point(472, 62);
+            this.ltbListaDesejos.Name = "ltbListaDesejos";
+            this.ltbListaDesejos.Size = new System.Drawing.Size(203, 264);
+            this.ltbListaDesejos.TabIndex = 4;
+            // 
+            // cbbEstados
+            // 
+            this.cbbEstados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbEstados.FormattingEnabled = true;
+            this.cbbEstados.ItemHeight = 20;
+            this.cbbEstados.Location = new System.Drawing.Point(67, 52);
+            this.cbbEstados.Name = "cbbEstados";
+            this.cbbEstados.Size = new System.Drawing.Size(174, 28);
+            this.cbbEstados.TabIndex = 5;
+            this.cbbEstados.TabStop = false;
+            // 
+            // lblDigiteEstado
+            // 
+            this.lblDigiteEstado.AutoSize = true;
+            this.lblDigiteEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigiteEstado.Location = new System.Drawing.Point(64, 163);
+            this.lblDigiteEstado.Name = "lblDigiteEstado";
+            this.lblDigiteEstado.Size = new System.Drawing.Size(118, 20);
+            this.lblDigiteEstado.TabIndex = 6;
+            this.lblDigiteEstado.Text = "Digite o Estado";
+            // 
+            // txtInserirEstado
+            // 
+            this.txtInserirEstado.Location = new System.Drawing.Point(67, 201);
+            this.txtInserirEstado.Name = "txtInserirEstado";
+            this.txtInserirEstado.Size = new System.Drawing.Size(190, 20);
+            this.txtInserirEstado.TabIndex = 1;
+            this.txtInserirEstado.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInserirEstado_KeyDown);
+            // 
+            // btnInserirEstado
+            // 
+            this.btnInserirEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserirEstado.Location = new System.Drawing.Point(67, 236);
+            this.btnInserirEstado.Name = "btnInserirEstado";
+            this.btnInserirEstado.Size = new System.Drawing.Size(144, 50);
+            this.btnInserirEstado.TabIndex = 2;
+            this.btnInserirEstado.Text = "Inserir Estado";
+            this.btnInserirEstado.UseVisualStyleBackColor = true;
+            this.btnInserirEstado.Click += new System.EventHandler(this.btnInserirEstado_Click);
+            // 
+            // frmEstruturaRepiticao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInserirEstado);
+            this.Controls.Add(this.txtInserirEstado);
+            this.Controls.Add(this.lblDigiteEstado);
+            this.Controls.Add(this.cbbEstados);
+            this.Controls.Add(this.ltbListaDesejos);
+            this.Controls.Add(this.lblListadeEstados);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnCarregarListadeDesejo);
+            this.Controls.Add(this.btnCarregaEstado);
+            this.Name = "frmEstruturaRepiticao";
+            this.Text = "frmEstruturaRepiticao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCarregaEstado;
+        private System.Windows.Forms.Button btnCarregarListadeDesejo;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblListadeEstados;
+        private System.Windows.Forms.ListBox ltbListaDesejos;
+        private System.Windows.Forms.ComboBox cbbEstados;
+        private System.Windows.Forms.Label lblDigiteEstado;
+        private System.Windows.Forms.TextBox txtInserirEstado;
+        private System.Windows.Forms.Button btnInserirEstado;
+    }
+}
---
+++ ProjetoOrientacaoObjeto/frmEstruturaRepiticao.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoOrientacaoObjeto
+{
+    public partial class frmEstruturaRepiticao : Form
+    {
+        //metodo construtor classe
+        public frmEstruturaRepiticao()
+        {
+            InitializeComponent();
+            //executar o método carrega comboEstados
+            carregaCombo();
+            //carregaListaEstado();*/
+            txtInserirEstado.Focus();
+        }
+
+        private void btnCarregaEstado_Click(object sender, EventArgs e)
+        {
+            carregaCombo();
+
+        }
+
+        private void carregaCombo()
+        {
+            cbbEstados.Items.Clear();
+            cbbEstados.Items.Add("Capão Redondo");
+            cbbEstados.Items.Add("São Luis");
+            cbbEstados.Items.Add("Campo Grande");
+            cbbEstados.Items.Add("Piraporinha");
+            cbbEstados.Items.Add("Jardim Angela");
+            cbbEstados.Items.Add("Vila Remo");
+            cbbEstados.Items.Add("Capelinha");
+            cbbEstados.Items.Add("Campo Limpo");
+            cbbEstados.Items.Add("Jardim Jacira");
+            cbbEstados.Items.Add("Jardim Nakamura");
+
+        }
+        private void btnCarregarListadeDesejo_Click(object sender, EventArgs e)
+        {
+            carregaListaEstado();
+        }
+
+        private void carregaListaEstado()
+        {
+            ltbListaDesejos.Items.Clear();
+            ltbListaDesejos.Items.Add("Pinheiros");
+            ltbListaDesejos.Items.Add("Socorro");
+            ltbListaDesejos.Items.Add("Ibirapuera");
+            ltbListaDesejos.Items.Add("Moema");
+            ltbListaDesejos.Items.Add("Borba Gato");
+            ltbListaDesejos.Items.Add("Granja Julieta");
+            ltbListaDesejos.Items.Add("Morumbi");
+            ltbListaDesejos.Items.Add("Alto da Boa Vista");
+            ltbListaDesejos.Items.Add("Vila Olimpia");
+            ltbListaDesejos.Items.Add("Brooklin");
+            ltbListaDesejos.Items.Add("Cidade Jardins");
+        }
+
+        private void btnInserirEstado_Click(object sender, EventArgs e)
+        {
+          inserirEstado();
+        }
+
+
+        public void inserirEstado()
+        {
+            try
+            {
+
+            ltbListaDesejos.Items.Add(txtInserirEstado.Text);
+            txtInserirEstado.Focus();
+            txtInserirEstado.Clear();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Favor Inserir valores.");
+                txtInserirEstado.Focus();
+                txtInserirEstado.Clear();
+            }
+        }
+
+        private void txtInserirEstado_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) 
+            {
+                inserirEstado();
+            }
+        }
+    }
+}
